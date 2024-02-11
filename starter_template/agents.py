bdaeec4cb1ecf9b9e2333929ab4caaac08f1d8db
Rocha Junior <56947430+rochajunior30@users.noreply.github.com>
2024-02-11 08:45:18 -0300

    Update agents.py

--- starter_template/agents.py
+++ starter_template/agents.py
@@ -13,24 +13,35 @@ class CustomAgents:
         self.OpenAIGPT4 = ChatOpenAI(model_name="gpt-4", temperature=0.7)
         self.Ollama = Ollama(model="openhermes")
 
-    def agent_1_name(self):
+    def agent_researcher(self):
         return Agent(
-            role="Define agent 1 role here",
-            backstory=dedent(f"""Define agent 1 backstory here"""),
-            goal=dedent(f"""Define agent 1 goal here"""),
-            # tools=[tool_1, tool_2],
-            allow_delegation=False,
+            role="Senior Researcher",
+            backstory=dedent(f"""You are an expert strategist who knows how to identify trends and emerging companies, technology, fintech and AI. You're great at finding interesting and exciting  news on internet. You transformed extracted data into detailed reports with more interesting titles and excerpts. You are an expert strategist who knows how to identify trends an You are great at finding interesting and exciting news about """+assunto+""". You've turned the extracted data into detailed reports"""),
+            goal=dedent(f"""Find and explore news about "+assunto+" on the most interesting and relevant Brazilian portals in 2024"""),
+            tools=[search_tool],
+            allow_delegation=True,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
 
-    def agent_2_name(self):
+    def agent_writer(self):
         return Agent(
-            role="Define agent 2 role here",
-            backstory=dedent(f"""Define agent 2 backstory here"""),
-            goal=dedent(f"""Define agent 2 goal here"""),
-            # tools=[tool_1, tool_2],
-            allow_delegation=False,
+            role="Senior Technical Writer",
+            backstory=dedent(f"""You are a writer specializing in technical innovation, business, bitcoin, fintechs and startups especially in the area of Marketing-AI and machine learning for marketing. do you know how to write engaging, interesting, yet simple, direct and concise. Do you know how to present complicated technical terms to the general public in a fun way using layman words. ONLY use data extracted from the internet for the post"""),
+            goal=dedent(f"""Write an engaging and interesting blog post about the latest news using simple, layman's vocabulary, translated into Brazilian Portuguese"""),
+            tools=[search_tool],
+            allow_delegation=True,
+            verbose=True,
+            llm=self.OpenAIGPT35,
+        )
+
+    def agent_chief_criative(self):
+        return Agent(
+            role="Chief Creative Director",
+            backstory=dedent(f"""You're the Chief Content Officer of leading digital marketing specialized in product branding. You're working on a new customer, trying to make sure your team is crafting the best possible content for the customer."""),
+            goal=dedent(f"""Oversee the work done by your team to make sure it's the best possible and aligned with the product's goals, review, approve, ask clarifying question or delegate follow up work if necessary to make decisions"""),
+            tools=[search_tool],
+            allow_delegation=True,
             verbose=True,
             llm=self.OpenAIGPT35,
         )
